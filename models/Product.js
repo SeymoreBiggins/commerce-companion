3491c082742dcf78c99056c2d3b7fc799eb6497a
Seymore Biggins <zachharris024@gmail.com>
2020-10-25 13:46:02 -0500

    initial commit

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {  // check if value is a deciaml
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {  // check it value is an integer
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {  // reference category model's id
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
